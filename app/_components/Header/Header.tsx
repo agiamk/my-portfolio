b780157dc9ff8c18d93a74416703a037a8982f80
kamada-taiga <km10150301@gmail.com>
2024-09-17 17:24:04 +0900

    header作成

--- app/_components/Header/Header.tsx
+++ app/_components/Header/Header.tsx
@@ -1,21 +1,24 @@
 "use client";
 
 import { Link as Scroll } from "react-scroll";
+import Sheet from "../Sheet";
 
 const Header: React.FC = () => {
   const navItem = ["Home", "About", "Portfolio", "Blog"];
 
   return (
-    <div className="flex">
-      <div>ポートフォリオ</div>
-      <nav>
-        {navItem.map((item) => (
-          <Scroll to={item} smooth key={item}>
-            {item}
-          </Scroll>
-        ))}
-      </nav>
-    </div>
+    <Sheet>
+      <div className="flex p-6 justify-between items-center">
+        <div>ポートフォリオ</div>
+        <nav className="flex justify-center items-center gap-7 underline underline-offset-2">
+          {navItem.map((item) => (
+            <Scroll to={item} smooth key={item} className="cursor-pointer">
+              {item}
+            </Scroll>
+          ))}
+        </nav>
+      </div>
+    </Sheet>
   );
 };
 
--- app/_components/Sheet/index.tsx
+++ app/_components/Sheet/index.tsx
@@ -2,10 +2,15 @@ import React from "react";
 
 type SheetProps = {
   children: React.ReactNode;
+  bgBlack?: boolean;
 };
 
-const Sheet = ({ children }: SheetProps) => {
-  return <div className="px-4 md:container mx-auto">{children}</div>;
+const Sheet = ({ children, bgBlack = false }: SheetProps) => {
+  return (
+    <div className={`${bgBlack && `bg-black text-white`}`}>
+      <div className="px-4 md:container mx-auto">{children}</div>
+    </div>
+  );
 };
 
 export default Sheet;
