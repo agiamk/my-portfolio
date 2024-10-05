1e11d41fd733a79c79870f71a06de88c6f820639
kamada-taiga <km10150301@gmail.com>
2024-10-06 01:52:02 +0900

    ヘッダーのマークアップ調整

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Header from "./_components/Header";
 export default function Home() {
   return (
     <div>
-      <Header isScroll />
+      <Header isScroll isTopPage />
       <About />
       <Blog />
       <Footer />
