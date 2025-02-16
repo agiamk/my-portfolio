257ef8987be09e17935a80cccb9d40ca0770ab1f
TAIGA\km101 <km10150301@gmail.com>
2025-02-17 00:21:09 +0900

    検索機能を実装

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,6 +3,7 @@ import { BLOG_LIST_LIMIT } from "../_constants";
 import BlogList from "../_components/BlogList";
 import Pagination from "../_components/Pagination";
 import SearchField from "../_components/SearchField";
+import { Suspense } from "react";
 
 const Page = async () => {
   const { contents: blogs, totalCount } = await getBlogList({
@@ -10,14 +11,16 @@ const Page = async () => {
   });
 
   return (
-    <div>
-      <h1 className="text-center text-2xl font-bold">Tech Blog</h1>
-      <div className="pt-4">
-        <SearchField />
-        <BlogList blogs={blogs} />
-        <Pagination totalCount={totalCount} />
+    <Suspense>
+      <div>
+        <h1 className="text-center text-2xl font-bold">Tech Blog</h1>
+        <div className="pt-4">
+          <SearchField />
+          <BlogList blogs={blogs} />
+          <Pagination totalCount={totalCount} />
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 };
 
